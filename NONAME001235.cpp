18b21b831a835e56e9602b29c17fcfeec471b8c8
kikimamun <35278800+kikimamun@users.noreply.github.com>
2018-01-10 09:37:13 +0700

    Add files via upload

---
+++ NONAME001235.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream.h>
+
+main()
+{
+float a,b,c,d,e;
+cout <<" Masukan Absen:";cin>>a;
+cout <<" Masukan Tugas:";cin>>b;
+cout <<" Masukan UTS:";cin>>c;
+cout <<" Masukan UAS:";cin>>d;
+a=a*0.2;
+b=b*0.25;
+c=c*0.25;
+d=d*0.3;
+
+e=a+b+c+d;
+
+cout <<"============================\n";
+cout <<"     Hasil Akhir            \n";
+cout <<"============================\n";
+cout <<"Nilai Absen:"<<a<<endl;
+cout <<"Nilai Tugas:"<<b<<endl;
+cout <<"Nilai UTS:"<<c<<endl;
+cout <<"Nilai UAS:"<<d<<endl;
+
+cout <<"\n Hasil Akhir					:"<<e<<ends;
+getch();
+}
---
+++ NONAME02.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream.h>
+
+main()
+{
+	char nama[7] ="ma'mun";
+	int nilai =100;
+	clrscr();
+
+	printf("hai %s, kamu mendapatkan nilai%i",nama, nilai);
+   getch();
+}
